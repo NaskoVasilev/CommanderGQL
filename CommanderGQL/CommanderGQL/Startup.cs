c12502ce495c5315798834c0643aa1f268debb7a
Atanas Vasilev <nasko01vasilev@gmail.com>
2022-04-15 14:17:42 +0300

    Initial project setup

---
+++ CommanderGQL/CommanderGQL/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace CommanderGQL
+{
+    public class Startup
+    {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+
+            app.UseRouting();
+        }
+    }
+}
