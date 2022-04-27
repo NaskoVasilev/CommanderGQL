df35422dd93068c6083f89eeda27acc4acb38a71
Atanas Vasilev <nasko01vasilev@gmail.com>
2022-04-27 13:13:06 +0300

    Added mutations

---
+++ CommanderGQL/CommanderGQL/GraphQL/Mutation.cs
@@ -0,0 +1,40 @@
+﻿using CommanderGQL.Data;
+using CommanderGQL.GraphQL.Commands;
+using CommanderGQL.GraphQL.Platforms;
+using CommanderGQL.Models;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Mutation
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        public async Task<AddPlatformPayload> AddPlatformAsync(AddPlatformInput input, [ScopedService] AppDbContext context)
+        {
+            var platform = new Platform
+            {
+                Name = input.Name,
+            };
+
+            await context.Platforms.AddAsync(platform);
+            await context.SaveChangesAsync();
+
+            return new AddPlatformPayload(platform);
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        public async Task<AddCommandPayload> AddCommnadAsync(AddCommandInput input, [ScopedService] AppDbContext context)
+        {
+            var command = new Command
+            {
+                HowTo = input.HowTo,
+                CommandLine = input.CommandLine,
+                PlatformId = input.PlatformId
+            };
+
+            await context.Commands.AddAsync(command);
+            await context.SaveChangesAsync();
+
+            return new AddCommandPayload(command);
+        }
+    }
+}
--- CommanderGQL/CommanderGQL/Models/Platform.cs
+++ CommanderGQL/CommanderGQL/Models/Platform.cs
@@ -12,10 +12,9 @@ namespace CommanderGQL.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
-        public string LicenseKey { get; set; }
+        public string? LicenseKey { get; set; }
 
         public ICollection<Command> Commands { get; set; }
     }
